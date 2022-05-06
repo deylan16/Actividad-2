3ad64ae14085d7bc324e34712ee4abf05186ad0b
Deylan Alexander <deylan6@gmail.com>
2022-05-05 18:31:47 -0600

    reloj

--- wep-app/src/App.js
+++ wep-app/src/App.js
@@ -1,17 +1,50 @@
+import React from 'react';
 import { useState } from 'react';
 import './App.css';
 import { fetchHelloWorld } from './services/HelloWorldService';
-function App() {
+/*function App() {
+  const [currentTime,setCurrentTime] = useState(Date.now());
   const [label, setLabel] = useState("");
   const clickHandler = async () => {
     const result = await fetchHelloWorld();
     setLabel(result);
 };
+
+setInterval(()=>{
+  setCurrentTime(Date.now())
+})
+
 return (
   <div className="App">
+    {new Date(currentTime).toLocaleString()}
     <button onClick={() => clickHandler()}>Call API</button>
     {label}
   </div>
 );
+}*/
+
+export class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentTime: Date.now()
+    }
+    setInterval(() => {
+      this.setState({
+        currentTime: Date.now()
+      });
+    }, 1000)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {new Date(this.state.currentTime).toLocaleString()}
+        <button onClick={() => this.clickHandler()}>Call API</button>
+        {this.state.label}
+      </div>
+    );
+  }
 }
+
 export default App;
